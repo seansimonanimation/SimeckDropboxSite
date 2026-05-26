917aec8f21db4f43a85edb9060df41b49d098643
Weeble Wobble <seansimonanimation@gmail.com>
2026-05-26 13:49:23 -0700

    elfinder visual fixes

--- modules/client/clientProjectManagement/module.php
+++ modules/client/clientProjectManagement/module.php
@@ -62,7 +62,7 @@ define('EF_ROOT', 'libraries/elfinder');
             cssAutoLoad: false,
             baseUrl: 'libraries/elfinder/',
             url: 'modules/client/clientProjectManagement/clientConnector.php',
-            height: $(window).height() - $('#elfinder').offset().top - 15,
+            height: $(window).height() - $('#elfinder').offset().top,
         });
         
         $(window).on('resize', resizeElfinder);
