10fdd10d01e7d90b5dc65cc4d72cbba15111195f
Weeble Wobble <seansimonanimation@gmail.com>
2026-05-27 14:46:00 -0700

    Clients can change their password now!

--- modules/client/clientSettings/module.php
+++ modules/client/clientSettings/module.php
@@ -105,7 +105,7 @@ function verifyCurrentPW($currentPW, $artistData){
         <div class="module-card module-card--span-1">
             <h1>Password change</h1>
             <form method="POST" class="module-create-form" action="">
-                <input class="module-input" type="hidden" name="ArtistChangePW" placeholder="Change Password" />
+                <input class="module-input" type="hidden" name="ClientChangePW" placeholder="Change Password" />
                 <input class="module-input" type="password" name="currentPW" placeholder="Current Password" required/><br />
                 <input class="module-input" type="password" name="newPW" placeholder="New Password" required/><br />
                 <input class="module-input" type="password" name="ConfirmNewPW" placeholder="Confirm New Password" required/><br />
